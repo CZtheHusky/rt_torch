fix a bug of loss calculation

rt_torch/dataset/dataset_npz.py
@@ -94,7 +94,7 @@ class language_table_dataset_npz(Dataset):
         self.rgb_list = rgb_list
         self.action_idx = np.arange(token_num - 1, seq_len * token_num, token_num)
         self.rgb_padding = torch.zeros(seq_len - 1, 3, 300, 300)
-        self.act_padding = torch.zeros(seq_len - 1, 2)
+        self.act_padding = torch.ones(seq_len - 1, 2)
         self.inst_embed_padding = torch.zeros(seq_len - 1, 768)
         self.inst_padding = [""] * (seq_len - 1)
         self.seed = seed
@@ -172,6 +172,7 @@ class language_table_dataset_npz(Dataset):
         self.weight = np.array(self.weight) / np.sum(self.weight)
         self.buffer = {"rgb": [], "inst": [], "act": [], "act_mask": []}
         self.buffer_len = 0
+        self.steps_per_batch = seq_len * batch_size
         assert len(self.weight) > 0
         print(self.ds_list)
                 
@@ -218,13 +219,99 @@ class language_table_dataset_npz(Dataset):
             self.buffer["act_mask"].extend(act_mask)
             self.ds_stats[ds_name]["current_idx"] += 1
             self.ds_stats[ds_name]["current_idx"] %= self.sub_size[ds_name]
-            
+
+    # def __loaditem__(self, ds=None):
+    #     while self.buffer_len < self.batch_size:
+    #         if ds is None:
+    #             choice = np.random.choice(len(self.weight), p=self.weight)
+    #             ds_name = self.ds_list[choice]
+    #         else:
+    #             ds_name = ds
+    #         npz_name = str(self.ds_stats[ds_name]["file_idx"][self.ds_stats[ds_name]["current_idx"] % self.sub_size[ds_name]] + self.index_range[ds_name][0]) + ".npz"
+    #         # print(f"accessing {ds_name}, idx: {npz_name}")
+    #         if self.embed_ready:
+    #             inst = np.load(os.path.join(self.ds_stats[ds_name]["path"]["inst_embed"], npz_name))['arr_0']
+    #         else:
+    #             inst = np.load(os.path.join(self.ds_stats[ds_name]["path"]["instruction"], npz_name))['arr_0']
+    #         rgb = np.load(os.path.join(self.ds_stats[ds_name]["path"]["rgb"], npz_name))['arr_0']
+    #         act = np.load(os.path.join(self.ds_stats[ds_name]["path"]["action"], npz_name))['arr_0']
+    #         split_num = int(ceil(len(rgb) / self.seq_len))
+    #         self.buffer_len += split_num
+    #         # print(f"add {len(rgb)} into buffer: {self.buffer_len}")
+    #         rgb, inst, act = self.return_preprocess(rgb, inst, act)
+    #         n = len(rgb) % self.seq_len
+    #         action_mask = torch.zeros(split_num, self.seq_len)
+    #         if n:
+    #             padding_num = self.seq_len - n
+    #             # import pdb; pdb.set_trace()
+    #             rgb = torch.cat([rgb, self.rgb_padding[:padding_num]])
+    #             action_mask[-1, -padding_num:] = 1
+    #             if self.embed_ready:
+    #                 inst = torch.cat([inst, self.inst_embed_padding[:padding_num]])
+    #             else:
+    #                 inst = inst.extend(self.inst_padding[:padding_num])
+    #             act = torch.cat([act, self.act_padding[:padding_num]])
+    #         # if len(rgb) % self.seq_len != 0 or len(inst) % self.seq_len != 0 or len(act) % self.seq_len != 0:
+    #         #     import pdb; pdb.set_trace()
+    #         self.buffer["rgb"].append(rgb)
+    #         self.buffer["inst"].append(inst)
+    #         self.buffer['act'].append(act)
+    #         self.buffer["act_mask"].append(action_mask)
+    #         self.ds_stats[ds_name]["current_idx"] += 1
+    #         self.ds_stats[ds_name]["current_idx"] %= self.sub_size[ds_name]
+        
 
     def return_preprocess(self, rgb, inst, act):
         if self.stack:
             return frame_stack(torch.tensor(rgb).float() / 255), torch.tensor(inst) if self.embed_ready else list(inst), torch.tensor(act)
         else:
             return torch.tensor(rgb).float() / 255, torch.tensor(inst) if self.embed_ready else list(inst), torch.tensor(act)
+
+    # def __getbuffer__(self):
+    #     self.buffer_len -= self.batch_size
+    #     tmp_buffer = {"rgb": [], "inst": [], "act": [], "act_mask": []}
+    #     # import pdb; pdb.set_trace()
+    #     # for k, v in self.buffer.items():
+    #     #     print(k)
+    #     #     for item in v:
+    #     #         print(item.shape)
+    #     if self.buffer_len != 0:
+    #         for k, v in self.buffer.items():
+    #             split_idx = self.buffer_len * self.seq_len
+    #             if k != "act_mask":
+    #                 tmp_buffer[k] = [v[-1][-split_idx:]]
+    #                 self.buffer[k][-1] = v[-1][:-split_idx]
+    #             else:
+    #                 tmp_buffer[k] = [v[-1][-self.buffer_len:]]
+    #                 self.buffer[k][-1] = v[-1][:-self.buffer_len]
+    #     # import pdb; pdb.set_trace()
+    #     # for k, v in self.buffer.items():
+    #     #     print(k)
+    #     #     for item in v:
+    #     #         print(item.shape)
+    #     # for k, v in tmp_buffer.items():
+    #     #     print(k)
+    #     #     for item in v:
+    #     #         print(item.shape)
+    #     act = torch.cat(self.buffer["act"])
+    #     rgb = torch.cat(self.buffer["rgb"])
+    #     if self.embed_ready:
+    #         inst = torch.cat(self.buffer["inst"])
+    #     else:
+    #         inst = self.buffer["inst"]
+    #     act_mask = torch.cat(self.buffer["act_mask"]).long()
+    #     # mask_idx = torch.where(act_mask != 0)
+    #     # import pdb; pdb.set_trace()
+    #     # act_mask = act_mask.numpy()
+    #     # mask_idx = mask_idx[0].numpy()
+    #     # bs_idx = torch.cat([torch.ones(int(act_mask[idx])) * idx for idx in mask_idx]).long()
+    #     # row_idx = torch.cat([torch.arange(self.seq_len - int(act_mask[idx]), self.seq_len) for idx in mask_idx]).long()
+    #     # # import pdb; pdb.set_trace()
+    #     # act_mask = tuple([bs_idx, row_idx])
+    #     self.buffer = tmp_buffer
+    #     # if len(rgb) != self.steps_per_batch or len(inst) != self.steps_per_batch or len(act) != self.steps_per_batch or len(rgb) != self.steps_per_batch:
+    #     #     import pdb; pdb.set_trace()
+    #     return rgb, inst, act, act_mask
 
     def __getbuffer__(self):
         self.buffer_len -= self.batch_size

rt_torch/rt1/rt_vanilla.py
@@ -47,6 +47,8 @@ class RT1_transformer(nn.Module):
         self.act_idx = torch.arange(self.num_learned_tokens - 1, seq_len * self.num_learned_tokens, self.num_learned_tokens)
         self.seq_len = seq_len
         self.memory_buffer = None
+        self.action_tokenizer = ActionTokenizer(num_action_bin=256, action_max=0.1, action_min=-0.1)
+        self.criterion = nn.CrossEntropyLoss()
 
     def forward(
             self,
@@ -54,8 +56,6 @@ class RT1_transformer(nn.Module):
             texts: Optional[List[str]] = None,
             texts_embeddings=None,
     ):
-        # depth = self.transformer_depth
-        bs = video.shape[0]
         video = rearrange(video, 'b t c h w -> (b t) c h w')
         device = video.device
         if texts_embeddings is None:
@@ -64,8 +64,25 @@ class RT1_transformer(nn.Module):
             texts_embeddings = rearrange(texts_embeddings, 'b t d -> (b t) d')
         image_tokens = self.image_tokenizer(video, texts_embeddings)
         # import pdb; pdb.set_trace()
-        image_tokens = rearrange(image_tokens, '(b t) n c -> b (t n) c', b=bs)
-        # attn_mask = repeat(attn_mask, 'i j -> (i r1) (j r2)', r1=self.num_learned_tokens, r2=self.num_learned_tokens)
+        image_tokens = rearrange(image_tokens, '(b t) n c -> b (t n) c', t=self.seq_len)
         logits = self.transformer(image_tokens, attention_mask=self.attn_mask.to(device), gather_idx=self.act_idx)
         # import pdb; pdb.set_trace()
         return logits
+    
+    def cal_loss(self,
+              data,
+              device,
+              ):
+        rgbs, instructions, actions, inst_embeddings, act_mask = data
+        rgbs = rgbs.to(device)
+        actions = actions.to(device)
+        actions_discretes = self.action_tokenizer.discretize(actions)
+        if inst_embeddings is not None:
+            inst_embeddings = inst_embeddings.to(device)
+        predicts = self.forward(video=rgbs, texts=instructions, texts_embeddings=inst_embeddings)
+        valid_idx = torch.where(act_mask == 0)
+        actions_discretes = actions_discretes.view(-1, self.seq_len, 2)
+        predicts = predicts[valid_idx].permute(0, 2, 1)
+        actions_discretes = actions_discretes[valid_idx]
+        loss = self.criterion(predicts, actions_discretes)
+        return loss

rt_torch/tokenizers/action_tokenizer.py
@@ -25,12 +25,13 @@ class ActionTokenizer:
         x = torch.clamp(x, -0.1, 0.1)
         # import pdb
         # pdb.set_trace()
-        x = ((x - self.min) / self.max / 2 * (self.num_action_bin - 1)).int().type(torch.int64) # x < num_action_bin
-        try:
-            x = F.one_hot(x, num_classes=self.num_action_bin)
-        except Exception as e:
-            import pdb
-            pdb.set_trace()
+        x = ((x - self.min) / self.max / 2 * (self.num_action_bin - 1)).long() # x < num_action_bin
+        # import pdb; pdb.set_trace()
+        # try:
+        #     x = F.one_hot(x, num_classes=self.num_action_bin)
+        # except Exception as e:
+        #     import pdb
+        #     pdb.set_trace()
         return x
 
     def discrete2Scalar(self, x):

train.py
@@ -255,38 +255,27 @@ def main(args):
     logger.info(f"\nTransformer:")
     getModelSize(model.transformer, logger)
 
-    action_tokenizer = ActionTokenizer(num_action_bin=256, action_max=0.1, action_min=-0.1)
-    criterion = nn.CrossEntropyLoss()
     # embedding_buffer = InstEmbeddingBuffer()
     print(f"split: train-{len(train_loader)}, test-{len(test_loader)}")
     train_loss = deque(maxlen=100)
 
-    # avg_time = {"data_prep": 0, "move_data": 0, "inference": 0, "gradient_step": 0}
+    avg_time = {"data_prep": 0, "inference": 0, "gradient_step": 0}
 
     for epoch in range(epoch_s, num_epoch):
         model.train()
 
-        # time0 = time.time()
+        time0 = time.time()
 
-        for rgbs, instructions, actions, inst_embeddings, act_mask in tqdm(train_loader):
+        for data in tqdm(train_loader):
 
-            # time1 = time.time()
+            time1 = time.time()
+            avg_time["data_prep"] += (time1 - time0)
 
-            rgbs = rgbs.to(device)
-            actions = actions.to(device)
-            actions_discretes = action_tokenizer.discretize(actions)
-            if inst_embeddings is not None:
-                inst_embeddings = inst_embeddings.to(device)
+            loss = model.cal_loss(data, device)
 
-            # time2 = time.time()
+            time2 = time.time()
+            avg_time["inference"] += (time2 - time1)
 
-            predicts = model(video=rgbs, texts=instructions, texts_embeddings=inst_embeddings)
-            valid_idx = torch.where(act_mask == 0)
-
-            # time3 = time.time()
-            # avg_time["data_prep"] += (time1 - time0)
-
-            loss = criterion(predicts[valid_idx], actions_discretes[valid_idx].float())
             optimizer.zero_grad()
             loss.backward()
             nn.utils.clip_grad_norm_(model.parameters(), norm_clip)
@@ -296,10 +285,8 @@ def main(args):
             elif scheduler is not None:
                 lr_scheduler.step()
 
-            # time0 = time.time()
-            # avg_time["move_data"] += (time2 - time1)
-            # avg_time["inference"] += (time3 - time2)
-            # avg_time["gradient_step"] += (time0 - time3)
+            time0 = time.time()
+            avg_time["gradient_step"] += (time0 - time2)
             
             loss_step += 1
             train_loss.append(loss.detach().cpu().numpy())
@@ -310,8 +297,8 @@ def main(args):
                 writer.add_scalar('Train_Loss', float(mean_loss), loss_step)
                 logger.info(f"EP: {epoch}, Loss step: {loss_step}, Loss: {mean_loss:.5f}")
 
-                # for key, value in avg_time.items():
-                #     logger.info(f"{key}: {(value / loss_step):.2f}")
+                for key, value in avg_time.items():
+                    logger.info(f"{key}: {(value / loss_step):.2f}")
 
             if save_interval != 0 and (loss_step) % save_interval == 0:
                 epoch_str = str(epoch)
@@ -343,14 +330,8 @@ def main(args):
                 with torch.no_grad():
                     indexes = np.random.randint(0, len(test_loader), size=1000)
                     for idx in indexes:
-                        rgbs, instructions, actions, inst_embeddings, act_mask = test_loader.__getitem__(idx)
-                        rgbs = rgbs.to(device)
-                        actions = actions.to(device)
-                        actions_discretes = action_tokenizer.discretize(actions)
-                    if inst_embeddings is not None:
-                        inst_embeddings = inst_embeddings.to(device)
-                        predicts = model(video=rgbs, texts=instructions, texts_embeddings=inst_embeddings)
-                        loss = criterion(predicts, actions_discretes.float())
+                        data = test_loader.__getitem__(idx)
+                        loss = model.cal_loss(data, device)
                         num_step += 1
                         test_loss += loss
                         if num_step % 100 == 0: